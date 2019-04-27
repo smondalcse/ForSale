49997a8095c35bd51eb27123aea1e33950f7f425
smondalcse <cse.sanat@gmail.com>
2019-04-27 21:03:00 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+<a href='https://www.youtube.com/watch?v=Z7ufbRuG9gY&list=PLgCYzUzKIBE-G0tuxjKGkl_keIW2FFwKX' target='_blank'>
+  <img class='header-img' src='https://s3.amazonaws.com/codingwithmitch-static-and-media/media/ForSale/Android+Classifieds+App.png' />
+</a>
+
+
+<h1>Android Classifieds App Course</h1>
+<h4>A step-by-step guide to build a fully functional app where users can post items or services for sale.</h4>
+<h4>Watch it here: <a href='https://www.youtube.com/watch?v=Z7ufbRuG9gY&list=PLgCYzUzKIBE-G0tuxjKGkl_keIW2FFwKX' target='_blank'>Classifieds App Course</a></h4>
+<hr>
+<p>
+Learn to to build a fully functional app where users can post items or services for sale. Other users can then search for those 
+items and services in their locality. </br>
+Since Firebase doesn't have any search methods where users can perform something similar to an "SQL LIKE" query, we'll be using
+ElasticSearch to index the data from Firebase for superior search functionality.
+</p> 
+<h2>Lecture Source Code:</h2>
+<ol>
+<li>Introduction</li>
+
+<li><a href='https://goo.gl/E7koEm'> Getting Started</a></li>
+
+<li><a href='https://goo.gl/E8dhMM'> Collapsing Toolbar Setup</a></li>
+
+<li><a href='https://goo.gl/F3JrQu'> Tabs Setup</a></li>
+
+<li><a href='https://goo.gl/GpQWNK'> Signing out a FirebaseUser</a></li>
+
+<li><a href='https://goo.gl/Ne6UFu'> Selecting an Image for Uploading</a></li>
+
+<li><a href='https://goo.gl/GwumRM'> Camera and Storage Permissions</a></li>
+
+<li><a href='https://goo.gl/D7sQ25'> Sending Data from Dialog to Fragment</a></li>
+
+<li><a href='https://goo.gl/3zHm9b'> Compressing Images in Android</a></li>
+
+<li><a href='https://goo.gl/HfZH7K'> Uploading an Image to Firebase Storage</a></li>
+
+<li><a href='https://goo.gl/ZFWvwK'> Posting Items for Sale</a></li>
+
+<li><a href='https://goo.gl/jLzPsS'> Rotating Images on Android</a></li>
+
+<li><a href='https://goo.gl/M4EJ2m'> Saving Search Preferences</a></li>
+
+<li> Deploying ElasticSearch</li>
+
+<li><a href='https://goo.gl/EufPna'>  Creating an ElasticSearch Index with Postman</a></li>
+
+<li><a href='https://goo.gl/QoYxS7'> Enabling Firebase Cloud Functions</a></li>
+
+<li><a href='https://goo.gl/qWLkWo'> ElasticSearch Firebase Cloud Function</a></li>
+
+<li><a href='https://goo.gl/uFHRVd'> Querying an ElasticSearch Server</a></li>
+
+<li><a href='https://goo.gl/JhwrGo'> Retrofit Setup in Android</a></li>
+
+<li><a href='https://goo.gl/MyAoeJ'> ElasticSearch Query Prep</a></li>
+
+<li><a href='https://goo.gl/H5hGnN'> ElasticSearch Android Retrofit Query</a></li>
+
+<li><a href='https://goo.gl/j7baHb'> RecyclerView Grid</a></li>
+
+<li><a href='https://goo.gl/vysTs1'> Inflating a FrameLayout</a></li>
+
+<li><a href='https://goo.gl/MCVynK'> Viewing Posts for Sale</a></li>
+
+<li><a href='https://goo.gl/wLsLkn'> Saving Posts to Watch-List</a></li>
+
+<li><a href='https://goo.gl/MWnuPu'>  Removing Posts from Watch-List</a></li>
+</ol>
---
+++ app/src/main/java/codingwithmitch/com/forsale/util/PostListAdapter.java
@@ -0,0 +1,119 @@
+package codingwithmitch.com.forsale.util;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import codingwithmitch.com.forsale.R;
+import codingwithmitch.com.forsale.SearchActivity;
+import codingwithmitch.com.forsale.SearchFragment;
+import codingwithmitch.com.forsale.WatchListFragment;
+import codingwithmitch.com.forsale.models.Post;
+
+/**
+ * Created by User on 11/1/2017.
+ */
+
+public class PostListAdapter extends RecyclerView.Adapter<PostListAdapter.ViewHolder>{
+
+    private static final String TAG = "PostListAdapter";
+    private static final int NUM_GRID_COLUMNS = 3;
+
+    private ArrayList<Post> mPosts;
+    private Context mContext;
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        SquareImageView mPostImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mPostImage = (SquareImageView) itemView.findViewById(R.id.post_image);
+
+            int gridWidth = mContext.getResources().getDisplayMetrics().widthPixels;
+            int imageWidth = gridWidth/NUM_GRID_COLUMNS;
+            mPostImage.setMaxHeight(imageWidth);
+            mPostImage.setMaxWidth(imageWidth);
+        }
+    }
+
+    public PostListAdapter(Context context, ArrayList<Post> posts) {
+        mPosts = posts;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.layout_view_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        UniversalImageLoader.setImage(mPosts.get(position).getImage(), holder.mPostImage);
+
+        final int pos = position;
+        holder.mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: selected a post");
+                //TODO
+
+                //view the post in more detail
+                Fragment fragment = (Fragment)((SearchActivity)mContext).getSupportFragmentManager()
+                        .findFragmentByTag("android:switcher:" + R.id.viewpager_container + ":" +
+                                ((SearchActivity)mContext).mViewPager.getCurrentItem());
+                if(fragment != null){
+                    //SearchFragment (AKA #0)
+                    if(fragment.getTag().equals("android:switcher:" + R.id.viewpager_container + ":0")){
+                        Log.d(TAG, "onClick: switching to: " + mContext.getString(R.string.fragment_view_post));
+
+                        SearchFragment searchFragment = (SearchFragment)((SearchActivity)mContext).getSupportFragmentManager()
+                                .findFragmentByTag("android:switcher:" + R.id.viewpager_container + ":" +
+                                        ((SearchActivity)mContext).mViewPager.getCurrentItem());
+
+                        searchFragment.viewPost(mPosts.get(pos).getPost_id());
+                    }
+                    //WatchList Fragment (AKA #1)
+                    else if(fragment.getTag().equals("android:switcher:" + R.id.viewpager_container + ":1")){
+                        Log.d(TAG, "onClick: switching to: " + mContext.getString(R.string.fragment_watch_list));
+
+                        WatchListFragment watchListFragment = (WatchListFragment)((SearchActivity)mContext).getSupportFragmentManager()
+                                .findFragmentByTag("android:switcher:" + R.id.viewpager_container + ":" +
+                                        ((SearchActivity)mContext).mViewPager.getCurrentItem());
+
+                        watchListFragment.viewPost(mPosts.get(pos).getPost_id());
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
